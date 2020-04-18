1cbb0da02e6423f30431f4044437438d789e58e4
Andy Barron <AndrewLBarron@gmail.com>
2020-04-17 22:19:44 -0700

    working list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -20,18 +20,24 @@ export function App() {
       <h1>Broaden your bar!</h1>
       <div>
         <h2>Ingredients</h2>
-        {[...ingredientMap.entries()].map(([id, { name }]) => {
-          return (
-            <label key={id}>
-              {name}{" "}
-              <input
-                type="checkbox"
-                checked={Boolean(checklist[id])}
-                onChange={() => toggleIngredient(id)}
-              />
-            </label>
-          )
-        })}
+        {[...[...ingredientMap.entries()].entries()].map(
+          ([index, [id, { name }]]) => {
+            const last = index === ingredientMap.size - 1
+            return (
+              <span>
+                <label key={id}>
+                  {name}{" "}
+                  <input
+                    type="checkbox"
+                    checked={Boolean(checklist[id])}
+                    onChange={() => toggleIngredient(id)}
+                  />
+                </label>
+                {!last && "  "}
+              </span>
+            )
+          },
+        )}
       </div>
     </div>
   )
